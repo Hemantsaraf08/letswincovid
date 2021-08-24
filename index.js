d7dbb976d98c529b56ec812f7442e2f2e45e1513
HEMANT SARAF <hemantsaraf08@gmail.com>
2021-08-25 01:07:24 +0530

    corrections in login.html

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express=require("express");
 const app=express();
 const PORT=process.env.PORT||3001;
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
+
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "pages","login.html"))
+})
+
+app.post("/profile", (req, res)=>{
+    console.log(req);
+    res.send("Here you are")
 })
